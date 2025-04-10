4169f24b906e49fb43325cd2a158df7e681416fa
Ramerlin <ramerlint2008@gmail.com>
2025-04-10 13:28:37 -0400

    Pre-config

---
+++ workflow-app/src/app/empleos/page.tsx
@@ -0,0 +1,7 @@
+export default function Empleos() {
+    return (
+        <div className="">
+            <h1 className="text-amber-950">Empleos</h1>
+        </div>
+    )
+}
---
+++ workflow-app/src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header (){
+    return (
+        <div className="fex">
+            <h1 className="bg-text-muted">Header</h1>
+        </div>
+    )
+}
