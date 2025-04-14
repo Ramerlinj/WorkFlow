ec8400a07ed00d09493d1076249d2d86da8f14a1
Ramerlin <ramerlint2008@gmail.com>
2025-04-13 23:27:06 -0400

    Config home page

--- workflow-app/src/app/page.tsx
+++ workflow-app/src/app/page.tsx
@@ -1,8 +1,16 @@
 
-export default function NamePage() {
+export default function HomePage() {
   return (
     <div>
-      <h1>Hello Page</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="text-lg text-gray-700">
+        This is a simple example of a Next.js page with Tailwind CSS.
+      </p>
+      <p className="text-lg text-gray-700">
+        You can start building your application from here.
+      </p>
     </div>
   );
 }
