47d63655415609d33e03bf96b35fca8ca502e7f1
Ramerlin <ramerlint2008@gmail.com>
2025-05-05 00:14:12 -0400

    Perfil refactorizado 1

--- workflow-app/src/app/[usuario]/_components/ModalEditProfile.tsx
+++ workflow-app/src/app/[usuario]/_components/ModalEditProfile.tsx
@@ -1,6 +1,114 @@
-export function ModalEdirProfile () {
-    return (
-        
-    )
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
+type ModalEditProfileProps = {
+  avatar: string | null;
+  first_name: string | null;
+  open: boolean;
+  setOpen: (value: boolean) => void;
+};
+
+export function ModalEditProfile({
+  avatar,
+  first_name,
+  open,
+  setOpen,
+}: ModalEditProfileProps) {
+  const [previewAvatar, setPreviewAvatar] = useState<string | null>(avatar);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageUrl = reader.result as string;
+      setPreviewAvatar(imageUrl);
+    };
+    reader.readAsDataURL(file);
+  };
+
+    const handleClose = () => {
+        setOpen(false);
+        setPreviewAvatar(avatar);
+    };
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-3xl max-h-[500px] overflow-y-auto p-4">
+        <DialogHeader>
+          <DialogTitle>Editar perfil</DialogTitle>
+          <DialogDescription>Edita el perfil a tu gusto.</DialogDescription>
+        </DialogHeader>
+
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <h4>Contenido adicional</h4>
+          </div>
+
+          <div>
+            <h4 className="font-semibold text-sm">Vista previa</h4>
+            <div className="w-72 h-72 bg-default-50 mt-2 rounded-lg shadow-lg">
+              <div className="bg-red-300 w-full h-32 rounded-t-lg relative">
+                <div className="absolute rounded-full h-24 w-24 bg-default-50 mb-10 top-18 ml-5">
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Avatar className="w-22 h-22 mt-1 mx-auto cursor-pointer hover:shadow-lg hover:opacity-80 transition-all duration-200 ease-in-out">
+                        <AvatarImage
+                          src={previewAvatar ?? undefined}
+                          className="w-full h-full object-cover"
+                        />
+                        <AvatarFallback className="bg-amber-500 text-3xl w-full h-full object-cover">
+                          {first_name ? first_name.charAt(0) : "?"}
+                        </AvatarFallback>
+                      </Avatar>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-72 p-4 bg-default-100 shadow-lg rounded-lg">
+                      <div className="flex flex-col gap-2">
+                        <h4 className="text-heading text-md">
+                          Cambiar foto de perfil
+                        </h4>
+                        <p className="text-secondary text-sm">
+                          Sube una nueva foto de perfil para actualizar tu
+                          imagen.
+                        </p>
+                        <Input
+                          className="cursor-pointer"
+                          type="file"
+                          accept="image/*"
+                          onChange={handleImageChange}
+                        />
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <DialogFooter>
+            <Button>Guadar cambios</Button>
+          <DialogClose asChild>
+            <Button variant="outline" onClick={handleClose}>Cerrar</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- workflow-app/src/app/[usuario]/_components/profile.tsx
+++ workflow-app/src/app/[usuario]/_components/profile.tsx
@@ -1,19 +1,26 @@
 'use client';
 
-import { Profile as ProfileTypes, User} from '@/types/user';
+import { useState } from 'react';
 import Image from 'next/image';
+import { MapPin, Mail, FileText, SquarePen } from 'lucide-react';
+import { Profile as ProfileTypes, User } from '@/types/user';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
-import { MapPin, Mail, FileText, SquarePen} from 'lucide-react';
 import { TabsDemo } from './Tabs';
 import SkillsCard from './SkillCard';
+import { ModalEditProfile } from './ModalEditProfile';
 import { LinksCard } from './LinksCard';
-import { Dialog, DialogTitle, DialogContent, DialogClose, DialogDescription, DialogHeader, DialogTrigger, DialogFooter } from '@/components/ui/dialog';
-import { useState } from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
-import { Input } from '@/components/ui/input';
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogClose,
+    DialogDescription,
+    DialogHeader,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+
 
 interface ProfileProps {
     profile: ProfileTypes;
@@ -22,11 +29,10 @@ interface ProfileProps {
 interface UserProps {
     user: User;
 }
-interface ProfileFullProps extends ProfileProps, UserProps { }
 
-function Profile({profile,user }: ProfileFullProps) {
+interface ProfileFullProps extends ProfileProps, UserProps {}
 
-    {/*const cv = profile.cv || null*/}
+function Profile({ profile, user }: ProfileFullProps) {
     const about_me = profile.about_me || null;
     const skills = user.skills || null;
     const first_name = user.first_name || null;
@@ -35,34 +41,25 @@ function Profile({profile,user }: ProfileFullProps) {
     const links = user.links || null;
     const workexperience = user.work_experience || null;
     const configuration = user.user_config || null;
-    {/*const address = user.address || null;*/}
 
     const [IsOpen, setIsOpen] = useState(false);
-    const [IsOpenPerfil, setIsOpenPerfil] = useState(false);
     const [avatar, setAvatar] = useState(profile.avatar);
+    const [IsOpenEdirProfile, setIsOpenEditProfile] = useState(false);
+
+    const handleEditProfile = () => {
+        setIsOpenEditProfile(true);
+    };
+
 
 
     const handleClick = () => {
         if (profile.cv) {
             window.open(profile.cv, '_blank');
-        }
-        else {
+        } else {
             setIsOpen(true);
         }
-    }
-
-    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (!file) return;
-
-        const reader = new FileReader();
-        reader.onloadend = () => {
-            const imageUrl = reader.result as string;
-            setAvatar(imageUrl);
-            
-        };
-        reader.readAsDataURL(file);
     };
+
     return (
         <div className="@container mx-8">
             <div className="h-96 w-full box-border">
@@ -75,11 +72,12 @@ function Profile({profile,user }: ProfileFullProps) {
                                     alt="Avatar"
                                     width={128}
                                     height={128}
-                                    className='w-full h-full object-cover'
+                                    className="w-full h-full object-cover"
                                 />
                             ) : (
-                                    <div className='flex w-full h-full object-cover text-center items-center justify-center bg-blue-700 text-4xl text-amber-50'>{ first_name ? first_name.charAt(0) : null }</div>
-                                
+                                <div className="flex w-full h-full object-cover text-center items-center justify-center bg-blue-700 text-4xl text-amber-50">
+                                    {first_name ? first_name.charAt(0) : null}
+                                </div>
                             )}
                         </div>
                     </div>
@@ -96,19 +94,24 @@ function Profile({profile,user }: ProfileFullProps) {
                                 Senior Developer
                             </Badge>
                         </div>
-                    <div className="flex justify-between items-center">
+                        <div className="flex justify-between items-center">
+                            <div className="flex flex-row gap-0.5 ml-4">
+                                <MapPin className="text-light w-5 h-auto" />
+                                <p className="text-light text-size-medium mr-5">
+                                    Agregar a la db direccion addres
+                                </p>
+                                <Mail className="text-light w-5 h-auto" />
+                                <p className="text-light">{email}</p>
+                            </div>
 
-                        <div className="flex flex-row gap-0.5 ml-4">
-                            <MapPin className='text-light w-5 h-auto' />
-                            <p className="text-light text-size-medium mr-5">Agregar a la db direccion addres</p>
-                            <Mail className='text-light w-5 h-auto'/>
-                            <p className="text-light">{email}</p>
-                        </div>
-
-                        <div className="flex gap-2 mr-6">
+                            <div className="flex gap-2 mr-6">
                                 <Dialog open={IsOpen} onOpenChange={setIsOpen}>
                                     <DialogTrigger asChild>
-                                        <Button variant="outline" size="sm" onClick={handleClick}>
+                                        <Button
+                                            variant="outline"
+                                            size="sm"
+                                            onClick={handleClick}
+                                        >
                                             <FileText /> Ver CV
                                         </Button>
                                     </DialogTrigger>
@@ -117,7 +120,8 @@ function Profile({profile,user }: ProfileFullProps) {
                                         <DialogHeader>
                                             <DialogTitle>CV no disponible</DialogTitle>
                                             <DialogDescription>
-                                                El usuario no ha subido un CV a su perfil, por lo que no se puede mostrar.
+                                                El usuario no ha subido un CV a su perfil, por lo que no se puede
+                                                mostrar.
                                             </DialogDescription>
                                         </DialogHeader>
                                         <DialogClose asChild>
@@ -126,112 +130,37 @@ function Profile({profile,user }: ProfileFullProps) {
                                     </DialogContent>
                                 </Dialog>
 
+                                <Button size="sm" onClick={handleEditProfile}>
+                                    <SquarePen /> Editar Perfil
+                                </Button>
+                                <ModalEditProfile
+                                    avatar={avatar}
+                                    first_name={first_name}
+                                    open={IsOpenEdirProfile}
+                                    setOpen={setIsOpenEditProfile}
+                                />
 
-                                <Dialog open={IsOpenPerfil} onOpenChange={setIsOpenPerfil}>
-                                    <DialogTrigger asChild>
-                                        <Button size="sm">
-                                            <SquarePen /> Editar Perfil
-                                        </Button>
-                                    </DialogTrigger>
-
-                                    <DialogContent className="sm:max-w-3xl max-h-[500px] overflow-y-auto p-4">
-                                        <DialogHeader>
-                                            <DialogTitle>Editar perfil</DialogTitle>
-                                            <DialogDescription>
-                                                Edita el perfil a tu gusto.
-                                            </DialogDescription>
-                                        </DialogHeader>
-
-                                        <div className="grid grid-cols-2 gap-4">
-                                            <div className="">
-                                                <h4>sd</h4>
-                                            
-                                            </div>
-                                            <div className="">
-                                                <h4 className='font-semibold text-sm'>Vista previa</h4>
-                                                <div  className="w-72 h-72 bg-default-50 mt-2 rounded-lg shadow-lg">
-                                                    
-                                                    <div className="bg-red-300 w-full h-32 rounded-t-lg relative">
-                                                        <div className="absolute rounded-full h-24 w-24 bg-default-50 mb-10 top-18 ml-5">
-                                                            <Popover>
-
-                                                            <PopoverTrigger asChild>
-                                                            <Avatar className='w-22 h-22 mt-1 mx-auto cursor-pointer hover:shadow-lg hover:opacity-80 transition-all duration-200 ease-in-out'>
-                                                                <AvatarImage  src={avatar ?? undefined} className='w-full h-full object-cover'></AvatarImage>
-                                                                <AvatarFallback className='bg-amber-500 text-3xl w-full h-full object-cover'>{first_name ? first_name.charAt(0) : null}</AvatarFallback>
-                                                            </Avatar>
-                                                            </PopoverTrigger>
-                                                            <PopoverContent className="w-72 p-4 bg-default-100 shadow-lg rounded-lg">
-                                                                <div className="flex flex-col gap-2">
-                                                                    <h4 className="text-heading text-md">Cambiar foto de perfil</h4>
-                                                                    <p className="text-secondary text-sm">Sube una nueva foto de perfil para actualizar tu imagen.</p>
-                                                                    <Input 
-                                                                    className='cursor-pointer'
-                                                                    type='file'
-                                                                    accept='image/*'
-                                                                    onChange={handleImageChange}
-                                                                    placeholder='Sube una nueva foto de perfil'
-                                                                    />
-
-                                                                </div>
-                                                            </PopoverContent>
-
-                                                            </Popover>
-
-                                                        </div>
-
-                                                    </div>
-
-                                                </div >
-
-                                            </div>
-
-                                        </div>
-                                        <div className="flex flex-col w-44 gap-2 mt-4">
-                                            {/*<Button>Guardar cambios</Button>
-                                            <Button>Cancelar</Button>*/}
-                                        </div>
-                                        <DialogFooter>
-                                            <DialogClose asChild>
-                                                <Button variant="outline">Cerrar</Button>
-                                            </DialogClose>
-                                        </DialogFooter>
-                                    </DialogContent>
-                                </Dialog>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
             <div className="flex flex-row justify-between items-start gap-4 mt-10">
                 <div className="flex flex-col gap-4 w-1/3">
-                    <Card className="">
+                    <Card>
                         <CardTitle className="text-heading text-md ml-5">Sobre Mí</CardTitle>
-                        <CardContent className="text-secondary text-sm">
-                            {about_me}
-                        </CardContent>
+                        <CardContent className="text-secondary text-sm">{about_me}</CardContent>
                     </Card>
 
                     <SkillsCard skills={skills} />
                     <LinksCard links={links} />
                 </div>
 
-                <TabsDemo workexperience={workexperience} UserConfig={configuration} />
-                
-                
+                <TabsDemo workexperience={workexperience} UserConfig={configuration}  />
             </div>
-
-        
-    </div>
-    
-
-
-
-
-
+        </div>
     );
 }
-
 
 export default Profile;
